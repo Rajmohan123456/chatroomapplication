dbb2a541bbe1b2e1eabe356ca39e43a3f505a642
Rajmohan123456 <rajmohandbg@gmail.com>
2024-11-13 13:10:25 +0530

    inital commit

---
+++ src/main/java/com/jsp/controller/MessageController.java
@@ -0,0 +1,17 @@
+package com.jsp.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jsp.models.Message;
+@RestController
+public class MessageController {
+    @MessageMapping("/message")
+    @SendTo("/topic/return-to")
+    public Message getContent(@RequestBody Message message) {
+
+        return message;
+    }
+}
